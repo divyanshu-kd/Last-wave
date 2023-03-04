dbcfddae28f1ee3a45bc0ddf558ae8f4ea49d1ab
Lakshay Nailwal <lakshaynailwal@gmail.com>
2023-03-04 17:04:28 +0530

    mainpage done

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-require("dotenv").config({path: "./config.env"})
+require("dotenv").config({ path: "./config.env" })
 
 const express = require("express");
 const cookieparser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer")
 const users = require("./models/user");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT||5000;
-const {checkUser,checkAuth} = require("./middleware/authmiddleware");
+const PORT = process.env.PORT || 5000;
+const { checkUser, checkAuth } = require("./middleware/authmiddleware");
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Conected to DB");
-    app.listen(PORT , ()=>{
+    app.listen(PORT, () => {
         console.log(`Runnning at port: ${PORT}`)
     })
-}).catch((e)=>{
+}).catch((e) => {
     console.log(e);
 })
 
@@ -30,51 +31,85 @@ app.set("view engine", "ejs");
 app.set("views", "templates");
 
 app.get('*', checkUser);
-app.get("/",(req,res)=>{res.render("index")});
-app.get("/checkup",(req,res)=>{res.render("app")});
+app.get("/", (req, res) => { res.render("index") });
+app.get("/checkup", (req, res) => { res.render("app") });
 
 
 // signup
 
-app.get("/signup",(req,res)=> {res.render("signup")})
-app.get("/login",(req,res)=> {res.render("login")})
+app.get("/signup", (req, res) => { res.render("signup") })
+app.get("/login", (req, res) => { res.render("login") })
 
 
 const maxage = 3 * 24 * 60 * 60;
-function createToken(id){
-    return jwt.sign({id}, process.env.SECRET , {expiresIn : maxage});
+function createToken(id) {
+    return jwt.sign({ id }, process.env.SECRET, { expiresIn: maxage });
 }
 
-app.post("/signup",async(req,res)=>{
+app.post("/signup", async (req, res) => {
 
-    const {username , password, email} = req.body;
+    const { username, password, email } = req.body;
 
-    const newUser = await users.create({username , password,email});
+    const newUser = await users.create({ username, password, email });
     const token = createToken(newUser._id);
 
-    res.cookie("jwt" , token , {maxAge : maxage * 1000 , httpOnly: true});
+    res.cookie("jwt", token, { maxAge: maxage * 1000, httpOnly: true });
     res.redirect("/");
 
 })
 
-app.post("/login",async(req,res)=>{
+app.post("/login", async (req, res) => {
 
-    const {email,passsword} = req.body;
+    const { email, passsword } = req.body;
 
-    const user = await users.findOne({email});
+    const user = await users.findOne({ email });
 
-    if(user.passsword === passsword){
+    if (user.passsword === passsword) {
         const token = createToken(user._id);
-        res.cookie("jwt",token , {maxAge:maxage * 1000 , httpOnly:true});
+        res.cookie("jwt", token, { maxAge: maxage * 1000, httpOnly: true });
         res.redirect('/')
     }
-    else{
+    else {
         res.redirect("/login");
     }
 
 })
 
-app.get("/logout",(req,res)=>{
-    res.cookie("jwt","frostHack" , {maxAge : 1, httpOnly:true});
+app.get("/logout", (req, res) => {
+    res.cookie("jwt", "frostHack", { maxAge: 1, httpOnly: true });
     res.redirect("/");
+})
+
+
+app.post("/send", (req, res) => {
+    const data = req.body;
+    console.log(data)
+
+    data.subject += `Message from ${data.email}`;
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        pool: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    })
+
+    const mailOptions = {
+        from: data.email,
+        to: "b21193@students.iitmandi.ac.in",
+        subject: data.subject,
+        text: data.body
+    }
+
+    let resData = { bool: true }
+    transporter.sendMail(mailOptions, (err, info) => {
+        transporter.close();
+        if (err) {
+            console.log(err)
+            resData.bool = false;
+        }
+    })
+    res.json(JSON.stringify(resData));
 })
